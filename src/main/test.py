e898527692d9267758b2ac315316872b622adb2c
Neeraj Kumar <kumar7neeraj.yadav@gmail.com>
2024-07-06 16:50:34 +0530

    clean up test file

--- src/main/test.py
+++ src/main/test.py
@@ -4,17 +4,24 @@ from model import UNet
 from data_helpers import reverse_transform
 from sampling import algo_two_simple
 
-model = UNet(channels = 1, dim = 28, dim_mults=[1,2])
-model.load_state_dict(torch.load("./2024-07-05-19:23:05.pth"))
-model.eval()
+def infer(saved_weights_path, save_as_name):
+    model = UNet(channels = 1, dim = 28, dim_mults=[1,2])
+    model.load_state_dict(torch.load(saved_weights_path))
+    model.eval()
 
-# i, n = generate_simple(model)
-# im = reverse_transform(i[0].detach())
-# print(type(im))
-# im.show()
 
-xs = algo_two_simple(model)
-for i in [1, 50, 100, 150, 200, 250, 299]:
-    im = reverse_transform(xs[i][0].detach())
-    im.save(f"run-5-{i}.jpg")
+    xs = algo_two_simple(model)
+    result = None
+    for i in [1, 50, 100, 150, 200, 250, 299]:
+        if result == None:
+            result = xs[i][0].detach()
+        else:
+            result = torch.cat([result, xs[i][0].detach()], axis = 2)
+
+    im = reverse_transform(result)
+    im.save(save_as_name)
+
+if __name__ == "__main__":
+    infer("./2024-07-05-19:23:05.pth", "results.jpg")
+    
 
